43488279ebc9ca7ac496db03b2777cedb06e9dd9
Abdul Hafeez <hafizabdulhafeez240302@gmail.com>
2024-06-09 17:17:32 +0530

    use brypt and JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,40 @@
 const express = require('express')
 const User = require('../models/User')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const { query, validationResult } = require('express-validator');
+
+const JWT_SECRET_KEY = "abdulh@afeez"
+
 const router = express.Router();
 
 //create user using: POST "/api/auth/". Does't require auth
-router.post('/', [
+router.post('/createuser', [
     query('name','Enter a valid Name').isLength({min : 4}),
     query('email', 'Enter a valid email').isEmail(),
     query('password','Password length must me greater than 5').isLength({min:5})
-],(req, res)=> {
+],async (req, res)=> {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
         return res.send({error: errors.array()});
     }
-    User.create({
+    const salt = await bcrypt.genSalt(10)
+    const secPass = await bcrypt.hash(req.body.password, salt)
+
+    let user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
 
-      }).then(user => res.json(user))
-      .catch(err=> {console.log(err)
-        res.json({error: 'please enter unique value for email'})});
-  
+    })
+    const data = {
+        user:{
+            id: user._id
+        }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET_KEY)
+
+    res.json({authtoken})
 })
 
 module.exports = router
